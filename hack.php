d5a35514c332652ea23eb30bc2935fd391f3828c
andalanta <51068832+andalanta@users.noreply.github.com>
2019-05-27 13:12:49 +0800

    Create hack.php

---
+++ hack.php
@@ -0,0 +1,16 @@
+<?php
+$banner = "\e[1;33m=====================
+      \e[0;32m###     ###
+    #        #   #
+     ###    ###
+       #   #   #
+   ###    ####
+     \e[0;35mSAKTI BAU
+\e[1;33m=====================\n";
+
+echo "$banner\n";
+echo "\e[0;34mnama saya \e[0;32msakti\n";
+echo "\e[0;34mtapi saya bau limpojong\n";
+echo "\e[0;34mtekan \e[0;31mCTRL Z \e[0;34muntuk keluar\n>
+
+?>
