46e6bb16c3f788cccdadfc6c39edb010af6a469e
andalanta <51068832+andalanta@users.noreply.github.com>
2019-05-27 14:49:26 +0800

    Update hack.php

--- hack.php
+++ hack.php
@@ -1,18 +1,11 @@
 <?php
 $banner = "\e[1;33m=====================
-sleep (0.3);
       \e[0;32m###     ###
-sleep (0.3);
     #        #   #
-sleep (0.3);
-     ###    ###
-sleep (0.3);
+     ###    ####
        #   #   #
-sleep (0.3);
    ###    ####
-sleep (0.3);
      \e[0;35mSAKTI BAU
-sleep (0.3);
 \e[1;33m=====================\n";
 
 echo "$banner\n";
@@ -27,5 +20,5 @@ echo "\e[0;31mHAHAHAHAHAAA\n";
 sleep (1);
 echo "\e[0;31mHA HA HA HA HA HA HAA\n";
 sleep (1);
-echo "\e[0;34mbau limpojong\n";
+echo "\e[0;34memabbbau limpojong\n";
 ?>
